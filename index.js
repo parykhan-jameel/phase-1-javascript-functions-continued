5d5c58cab9fb9494d4587f167858e858eddac879
parykhan-jameel <parexan.00443431@gmail.com>
2022-10-15 01:21:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+const saturdayFun = (activity=`roller-skate`) =>{
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun(`bathe my dog`);
+
+const mondayWork = (plans=`go to the office`) =>{
+    return(`This Monday, I will ${plans}.`);
+}
+mondayWork(`work from home`);
+
+const wrapAdjective = (praise = "*") => {
+    return function encouragingPrompt(morePraise = "special") {
+    // return encouragingPrompt = (morePraise = `special`) => {
+        return `You are ${praise}${morePraise}${praise}!`
+    }
+
+}
+
+wrapAdjective()
